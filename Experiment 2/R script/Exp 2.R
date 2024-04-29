5bf4f65129d3887ac7aa70602d45236ec6fdb263
JunningPeng <108398127+JunningXX@users.noreply.github.com>
2024-04-29 08:39:02 +0100

    Add files via upload

--- Experiment 2/R script/Exp 2.R
+++ Experiment 2/R script/Exp 2.R
@@ -35,8 +35,8 @@ study2 <- study2 %>%
   mutate(pic.match = ifelse(study2$paired_picture==study2$Picture,'Correct','Incorrect'))
 ### Change char to factor/numeric
 # study2$`vaccine (Yes/No)` <-  as.factor(study2$`vaccine (Yes/No)`)
-study2$condition <-  as.factor(study2$condition)
-study2$Sex <-  as.factor(study2$Sex)
+study2$condition <-  factor(study2$condition, levels = c("unmask", "mask"))
+study2$Sex <-  factor(study2$Sex)
 study2$Picture <-  as.factor(study2$Picture)
 study2$paired_picture <-  as.factor(study2$paired_picture)
 study2$Picturelabel <-  as.factor(study2$Picturelabel)
@@ -48,6 +48,15 @@ study2$behaviour <-  factor(study2$behaviour,levels = c("As frequently","Much le
 study2$party <- factor(study2$party, levels = c("Conservative Party", "Labour Party", "Liberal Democrats", 
                                                 "Scottish National Party", "Other Party (Please specify)", "Did not vote", 
                                                 "Prefer not to say"))
+study2 <- study2 %>% mutate(protection_simple = ifelse(protection == "Only themselves" | protection == "Mostly themselves", "themselves",
+                                                       ifelse(protection == "Equally themselves and other", 
+                                                              "Equally themselves and other", "others"))) %>% 
+  mutate(behaviour_simple = ifelse(behaviour == "Much less frequently" | behaviour == "Less frequently", 
+                                   "less", "equally or more"))
+
+study2$protection_simple <- as.factor(study2$protection_simple)
+study2$behaviour_simple <- factor(study2$behaviour_simple, levels = c("equally or more", "less"))
+
 
 # Get rid of participants who got wrong with the comprehension question (the first two)
 cleandata <- study2 %>% filter(Comprehension1 == "£9" & Comprehension2 == "£2" & pic.match == "Correct") # 361 observation
@@ -57,61 +66,6 @@ cleandata <- study2 %>% filter(Comprehension1 == "£9" & Comprehension2 == "£2"
 cleandata_4Comp <- study2 %>% filter(Comprehension1 == "£9" & Comprehension2 == "£2" & pic.match == "Correct", 
                                       Comp_Sender_Total == "£9  (Initial £10 minus £3 sent plus £2 received from Responder)" & 
                                       Comp_Responder_Total == "£17  (Initial £10 plus £9 received from Sender minus £2 returned)")
-
-
-# ks test                     
-LT<- cleandata %>% filter(trustworthylevel == "low") %>% select(mean_return)
-LT <- as.data.frame(LT)
-MT<- cleandata %>% filter(trustworthylevel == "medium") %>% select(mean_return)
-MT <- as.data.frame(MT)
-HT<- cleandata %>% filter(trustworthylevel == "high") %>% select(mean_return)
-HT <- as.data.frame(HT)
-ks.test(LT[,1],MT[,1])
-ks.test(LT[,1],HT[,1])
-cohen.d(LT[,1],HT[,1])
-
-# Correlation plots 
-corr_plots <- function(data, x, y, ...){
-  ggplot(data = data, aes_string({{x}}, {{y}}, ...)) +
-    geom_jitter() +
-    geom_smooth(aes_string({{x}}, {{y}}), method = "lm") +
-    labs(x = x, y = y) +
-    stat_cor(method="pearson")
-}
-
-a1 <- corr_plots(cleandata, "attractiveness", "trustworthy") + 
-  facet_wrap(~condition)
-a2 <- corr_plots(cleandata, "Sender", "trustworthy")
-a3 <- corr_plots(cleandata, "mean_return", "trustworthy")
-cowplot::plot_grid(a1,a2,a3, ncol=2,labels = c("A","B","C"))
-
-### Vaccine percentage
-ggplot(cleandata, aes(x = `vaccine`)) +
-  geom_bar(alpha = .8) +
-  geom_text(stat='count', aes(label=..count..), vjust=-0.3) + 
-  ggtitle("Vaccination proportion") +
-  labs(x="Vaccine (Yes/No)")
-
-### Picture selection 
-ggplot(cleandata, aes(x = pic.match)) +
-  geom_bar(alpha = .8) +
-  geom_text(stat='count', aes(label=..count..), vjust=-0.3) +
-  labs(x = "Picture Selection") + 
-  ggtitle("Picture Match") +
-  labs(x="Whether participants selected the correct model")
-
-### Comprehension question
-## Mask wearing behaviour 
-ggplot(cleandata, aes(x = behaviour, fill = protection)) +
-  geom_bar(alpha = .4) +
-  labs(x = "Mask wearing behaviour after lockdown", fill="Protection Attitude") +
-  ggtitle("Mask wearing behaviour and participant's attitude") 
-
-### Voting party
-ggplot(cleandata, aes(x = party, fill = protection)) +
-  geom_bar(alpha = .4) +
-  labs(x = "Voting party", fill="Protection Attitude") +
-  ggtitle("Voting from last election") 
 
 
 ### Responder -- transfer from money value to proportion value
@@ -139,7 +93,7 @@ cleandata_4Comp$Responder30 <- cleandata_4Comp$Responder30/30
 
 
 ### converting data from wide to long 
-Data_long <- cleandata_4Comp %>% pivot_longer(cols = Responder3:Responder30, names_to = "Responder_value",
+Data_long <- cleandata %>% pivot_longer(cols = Responder3:Responder30, names_to = "Responder_value",
                                         values_to = "Pound_returned")
 Data_long$Pound_returned <- as.numeric(Data_long$Pound_returned)
 
@@ -147,23 +101,11 @@ Data_long$Responder_value <- factor(Data_long$Responder_value, levels = c("Respo
                                                                           "Responder15","Responder18","Responder21","Responder24",
                                                                           "Responder27","Responder30"))
 
+
 fre <- Data_long %>%
   group_by(Sender, Responder_value, condition) %>%
   summarise(n = n()) %>%
   mutate(freq = n / sum(n))
-
-### calculate mean proportion returned 
-cleandata <- cleandata %>% mutate(mean_return = as.vector(rowMeans(cleandata[, 9:18])))
-
-cleandata <- cleandata %>%
-  mutate(amountreturned = ifelse(cleandata$mean_return <= 0.33,"low",
-                                   ifelse(cleandata$mean_return >= 0.34 & cleandata$mean_return < 0.66, "medium", "high")))
-
-cleandata$amountreturned <- factor(cleandata$amountreturned, levels = c("low", "medium", "high"))
-
-cor(cleandata$trustworthy, cleandata$mean_return)
-cor(cleandata$trustworthy, cleandata$Sender)
-cor(cleandata$mean_return, cleandata$Sender)
 
 ### Linear Regression 
 library(ggfortify)
@@ -181,8 +123,9 @@ skewness(cleandata$trustworthy)
 
 ### Sender 
 set_default_contrasts()
-lm1 <- lm(Sender ~ condition + Sex + Order + Sex * condition , cleandata_4Comp)
-tab_model(lm1, p.style = "numeric_stars", show.aic = T, auto.label = TRUE)
+lm1 <- lm(Sender ~ condition + Sex + Order + Sex * condition , cleandata)
+tab_model(lm1, p.style = "numeric_stars", show.aic = T, auto.label = TRUE, 
+          show.stat = TRUE, show.se = TRUE)
 summary(lm1)
 
 #                           Estimate Std. Error t value Pr(>|t|)    
@@ -197,7 +140,7 @@ summary(lm1)
 # Residual standard error: 3.191 on 356 degrees of freedom
 # Multiple R-squared:  0.06535,	Adjusted R-squared:  0.05485 
 # F-statistic: 6.222 on 4 and 356 DF,  p-value: 7.538e-05
-set_sum_contrasts()
+
 lm2 <- lm(Sender ~ condition + Sex + Order + Sex * condition + cont_behave*condition , cleandata)
 tab_model(lm2, p.style = "numeric_stars", show.aic = T, auto.label = TRUE)
 
@@ -206,25 +149,21 @@ lm3 <- lm(Sender ~ Order + behaviour + trustworthy, cleandata)
 lm4 <- lm(Sender ~ condition  + Sex + Order + behaviour + trustworthy, cleandata)
 
 # Exploratory model 
-lm5 <- lm(Sender ~ Sex + condition + Order  + behaviour + protection + trustworthy +
+lm5 <- lm(Sender ~ Sex + condition + Order + behaviour_simple + protection_simple + trustworthy +
             condition*Sex + condition*Order + Sex*Order + 
             condition*trustworthy, cleandata)
-tab_model(lm5, p.style = "numeric_stars", show.aic = T, auto.label = TRUE)
+summary(lm5)
+tab_model(lm5, p.style = "numeric_stars", show.aic = T, auto.label = TRUE, 
+          show.stat = TRUE, show.se = TRUE)
 
-Reg_table <- mtable("Model 1"=lm1, "Model 5"=lm5,
-                    summary.stats=c("sigma","R-squared","F","p","N"))
-
-Mask <- cleandata%>%filter(condition=="unmask")
-Unmask <- cleandata%>%filter(condition=="mask")
+Mask <- cleandata%>%filter(behaviour_simple=="less")
+Unmask <- cleandata%>%filter(behaviour_simple!="less")
 cohen.d(Mask$Sender,Unmask$Sender)
+
 ### Calculating effect size
 SF <- cleandata%>%filter(Order=="SenderFirst")
 RF <- cleandata%>%filter(Order=="ResponderFirst")
 cohen.d(SF$Sender,RF$Sender)
-
-H <- cleandata%>%filter(trustworthylevel=="high")
-L <- cleandata%>%filter(trustworthylevel == "low")
-cohen.d(H$Sender,L$Sender)
 
 BehLess <- cleandata%>%filter(behaviour=="Much less frequently")
 BehAs <- cleandata%>%filter(behaviour == "As frequently")
@@ -244,31 +183,16 @@ lm4 <- lm(trustworthy ~ condition  + Sex + Order + sentamount+ behaviour+ amount
 # Exploratory Model
 lm5 <- lm(trustworthy ~ Sex + condition + Order  + behaviour + protection +
             condition*Sex + condition*Order + Sex*Order, cleandata)
-tab_model(lm5, p.style = "numeric_stars", show.aic = T, auto.label = TRUE)
-
-Reg_table2 <- mtable("Model 1"=lm1,"Model 5"=lm5,
-                    summary.stats=c("sigma","R-squared","F","p","N"))
-Reg_table2
-
+tab_model(lm5, p.style = "numeric_stars", show.aic = T, auto.label = TRUE, 
+          show.stat = TRUE, show.se = TRUE)
 
 ### Calculating effect size
 M <- cleandata%>%filter(condition=="mask")
 U <- cleandata%>%filter(condition=="unmask")
 cohen.d(M$trustworthy,U$trustworthy)
 
-M <- cleandata%>%filter(sentamount=="high")
-L <- cleandata%>%filter(sentamount == "low")
-cohen.d(M$trustworthy,L$trustworthy)
-
-H <- cleandata%>%filter(amountreturned=="medium")
-L <- cleandata%>%filter(amountreturned == "low")
-cohen.d(H$trustworthy,L$trustworthy)
-
 mean(cleandata[cleandata$condition == "mask", 25])
 mean(cleandata[cleandata$condition == "unmask", 25])
-autoplot(lm5)
-# Residual Histogram
-ols_plot_resid_hist(lm5)
 
 # Follow-up test 
 em4 <- emmeans(lm5, c("condition","Order"), model = "multivariate")
@@ -312,46 +236,8 @@ Reg_table3 <- mtable("Model 1"=lm1,"Model 5"=lm5,
                      summary.stats=c("sigma","R-squared","F","p","N"))
 Reg_table3
 summary(lm1)
-tab_model(lm1, p.style = "numeric_stars", show.aic = T, auto.label = TRUE)
-
-## Effect size
-M <- cleandata%>%filter(sentamount=="high")
-L <- cleandata%>%filter(sentamount == "low")
-cohen.d(M$attractiveness,L$attractiveness)
-
-H <- cleandata%>%filter(amountreturned=="high")
-L <- cleandata%>%filter(amountreturned == "low")
-cohen.d(H$attractiveness,L$attractiveness)
-
-# Follow-up test 
-em5 <- emmeans(lm5, c("condition","Order"), model = "multivariate")
-con5 <-  
-  list(ResF_M_v_U = c(1,-1,0,0),
-       SeF_M_v_U = c(0,0,1,-1),
-       Mask_ResF_v_SeF = c(1,0,-1,0),
-       Umask_ResF_v_SeF = c(0,1,0,-1))
-c5 <- contrast(em5, con5, adjust = "holm") %>% as_tibble()
-
-# A tibble: 4 x 6
-# contrast         estimate    SE    df t.ratio p.value
-# <chr>               <dbl> <dbl> <dbl>   <dbl>   <dbl>
-# 1 ResF_M_v_U           3.42  2.96   351    1.16   0.461
-# 2 SeF_M_v_U           -5.03  3.00   351   -1.68   0.376
-# 3 Mask_ResF_v_SeF      4.85  3.03   351    1.60   0.376
-# 4 Umask_ResF_v_SeF    -3.61  3.01   351   -1.20   0.461
-
-eff5 <- as.data.frame(effect(term="condition*Order", mod=lm5))
-ggplot(data=eff5, aes(x=Order, y=fit, group=condition, fill = condition)) + 
-  geom_point() +
-  geom_line() +
-  geom_ribbon(aes(ymin=lower, ymax=upper), alpha=.1) +
-  geom_errorbar(aes(ymin=lower, ymax=upper), width=.15) + 
-  xlab("Order") + 
-  ylab("Attractiveness") 
-
-ResFM <- cleandata%>%filter(Order=="ResponderFirst"&condition=="mask")
-ResFU <- cleandata%>%filter(Order=="ResponderFirst"&condition=="unmask")
-cohen.d(ResFM$attractiveness,ResFU$attractiveness)
+tab_model(lm1, p.style = "numeric_stars", show.aic = T, auto.label = TRUE, 
+          show.stat = TRUE, show.se = TRUE)
 
 
 ### Multilevel Regression 
@@ -380,26 +266,24 @@ model5 <- lme(fixed = Pound_returned~condition + Sex + Order + Sender + conditio
 
 summary(model5)
 tab_model(model5, p.style = "numeric_stars", show.aic = T, auto.label = TRUE)
+
+coeficients <- coef(model5)
+intervals(model5)
+
 SF <- Data_long%>%filter(condition=="unmask")
 RF <- Data_long%>%filter(condition=="mask")
 cohen.d(SF$Pound_returned,RF$Pound_returned)
 
+SF <- Data_long%>%filter(Order=="SenderFirst")
+RF <- Data_long%>%filter(Order=="ResponderFirst")
+cohensD(SF$Pound_returned,RF$Pound_returned)
+
 model6 <- lme(fixed = Pound_returned~condition + Sex + Order + Sender + trustworthy + 
-                protection + behaviour + condition*Sex + behaviour*condition, 
+                protection_simple + behaviour_simple + condition*Sex, 
               random = ~Sender|ID, data = Data_long)
 summary(model6)
-tab_model(model6, p.style = "numeric_stars", show.aic = T, auto.label = TRUE)
-
-Csender <- Data_long$Sender - mean(Data_long$Sender) 
-
-Data_long <-Data_long %>% mutate(Csender = Data_long$Sender - mean(Data_long$Sender))
-
-LT<- Data_long %>% filter(trustworthylevel == "low") %>% select(Pound_returned)
-MT<- Data_long %>% filter(trustworthylevel == "medium") %>% select(Pound_returned)
-HT<- Data_long %>% filter(trustworthylevel == "high") %>% select(Pound_returned)
-
-cohen.d(MT$Pound_returned,LT$Pound_returned)
-cohen.d(HT$Pound_returned,LT$Pound_returned)
+tab_model(model6, p.style = "numeric_stars", show.aic = T, auto.label = TRUE, 
+          show.stat = TRUE, show.se = TRUE)
 
 anova(null,model2,model3,model4,model5)
 
@@ -433,7 +317,7 @@ plot4 <- CI_sender_2 %>%
     y = "Amount sent by trustor",
     subtitle = "Exp 2 (Female Counterparts Only)",
     fill = "Mask",
-    caption = "      Error bars indicate standard errors"
+    caption = "      Error bars indicate standard errors of the mean"
   ) + 
   scale_fill_grey(start = 0.3) +
   theme_classic() +
@@ -456,6 +340,9 @@ plot4 <- CI_sender_2 %>%
         axis.text = element_text(size = 14),
         axis.title=element_text(size=14,face="bold"))
 
+cowplot::plot_grid(plot1, plot4,ncol=1,nrow = 2, rel_heights = c(1.6,1))
+
+
 CI_trust_2 <- cleandata %>% group_by(Sex, condition) %>% 
   summarise(m = mean(trustworthy),
             se = sd(trustworthy) / sqrt(length(trustworthy))) %>% 
@@ -477,7 +364,7 @@ plot5 <- CI_trust_2 %>%
     y = "Trustworthiness Ratings",
     fill = "Mask",
     subtitle = "Exp 2 (Female Counterparts Only)",
-    caption = "Error bars indicate standard error"
+    caption = "Error bars indicate standard errors of the mean"
   ) + 
   scale_fill_grey(start = 0.3) +
   theme(plot.title = element_text(size = 16,
@@ -540,7 +427,7 @@ plot6 <- ggplot(CI_return2) +
     y = "Proportion returned",
     subtitle = "Exp 2 (Female Counterparts Only)",
     colour = "Mask manipulation",
-    caption = "Error bars indicate standard error"
+    caption = "Error bars indicate standard errors of the mean"
   ) +
   scale_colour_manual(values=c("Black", "gray60")) +
   facet_wrap(~Sex) +
